4105af934786c715615bfb88da6daf72b4f921e3
TheRootOf3 <aceszablewski@gmail.com>
2023-09-26 13:38:07 +0100

    Added gh-pages CD workflows

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -23,7 +23,7 @@ const config = {
 
   trailingSlash: false,
 
-  onBrokenLinks: "throw",
+  onBrokenLinks: "warn",
   onBrokenMarkdownLinks: "warn",
 
   // Even if you don't use internalization, you can use this field to set useful
